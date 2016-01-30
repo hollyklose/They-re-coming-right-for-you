ba9280feea2b2253b60c17e470ff72d7f72a8db1
Holly Klose <hollyklose@gmail.com>
2016-01-30 11:17:18 -0700

    Initial Commit

---
+++ They're Coming Right For YouTests/They_re_Coming_Right_For_YouTests.swift
@@ -0,0 +1,36 @@
+//
+//  They_re_Coming_Right_For_YouTests.swift
+//  They're Coming Right For YouTests
+//
+//  Created by Holly Klose on 1/30/16.
+//  Copyright © 2016 Holly Klose. All rights reserved.
+//
+
+import XCTest
+@testable import They_re_Coming_Right_For_You
+
+class They_re_Coming_Right_For_YouTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

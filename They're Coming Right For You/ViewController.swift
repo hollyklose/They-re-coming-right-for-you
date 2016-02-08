ae848008d2acf920b3bf19a96b732cfd8252a151
Holly Klose <hollyklose@gmail.com>
2016-02-08 14:01:31 -0700

    first commit to git

--- They're Coming Right For You/ViewController.swift
+++ They're Coming Right For You/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redcat: UIImageView!
+    @IBOutlet weak var bluecat: UIImageView!
+
+    @IBOutlet weak var cryingcat: UIImageView!
+    
+    @IBOutlet weak var redbutton: UIButton!
+    @IBOutlet weak var bluebutton: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func byeredcat(sender: AnyObject) {
+        redcat.hidden = true
+        if bluecat.hidden == true { cryingcat.hidden = false
+        }
+    }
 
+    @IBAction func byebluecat(sender: AnyObject) {
+        bluecat.hidden = true
+        if redcat.hidden == true { cryingcat.hidden = false
+        }
+        
+    }
+    
+    
 }
 
